979cff8d30f6851d25e93e945d54ca2f27657da1
Jonathan Eduardo <jonnymartinez.pzl1@gmail.com>
2023-09-24 04:38:45 -0600

    Comenzando proyecto api travel

---
+++ src/main/java/com/jonhdevelop/springbootapitravel/SpringbootApiTravelApplication.java
@@ -0,0 +1,13 @@
+package com.jonhdevelop.springbootapitravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApiTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApiTravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonhdevelop/springbootapitravel/SpringbootApiTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonhdevelop.springbootapitravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApiTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
